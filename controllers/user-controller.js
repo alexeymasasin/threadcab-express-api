5ba42dcb2af634a9aeea44deac0238496e63f61e
Alexey Masasin <alexeymasasin@gmail.com>
2024-12-23 09:23:39 +0500

    User controller - current user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -147,7 +147,34 @@ const UserController = {
         }
     },
     current: async (req, res) => {
-        res.send("current");
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: req.user.userId,
+                },
+                include: {
+                    followers: {
+                        include: {
+                            follower: true,
+                        },
+                    },
+                    following: {
+                        include: {
+                            following: true,
+                        },
+                    },
+                },
+            });
+
+            if (!user) {
+                return res.status(400).json({ error: "User not found" });
+            }
+
+            res.json(user);
+        } catch (error) {
+            console.log(`Error in current. ${error}`);
+            res.status(500).json({ error: "Internal server error" });
+        }
     },
 };
 
