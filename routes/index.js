d91f3420d90a68c51aa184f0fd4d4461c5b358d5
Alexey Masasin <alexeymasasin@gmail.com>
2024-12-23 06:33:04 +0500

    Middleware, it's integration

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const { UserController } = require("../controllers");
+const authentificateToken = require("../middleware/auth");
 
 const uploadDestination = "uploads";
 
 // Показываем, где хранить файлы
 const storage = multer.diskStorage({
-  destination: uploadDestination,
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+    destination: uploadDestination,
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    },
 });
 
 const uploads = multer({ storage: storage });
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
-router.get("/current", UserController.current);
-router.get("/users/:id", UserController.getUserById);
-router.put("/users/:id", UserController.updateUser);
+router.get("/current", authentificateToken, UserController.current);
+router.get("/users/:id", authentificateToken, UserController.getUserById);
+router.put("/users/:id", authentificateToken, UserController.updateUser);
 
 module.exports = router;
